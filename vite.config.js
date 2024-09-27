1942035a74136e6b131a48d5b87066831c36952a
Yaroslav Slashev <slasev903@gmail.com>
2024-09-27 20:50:45 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['public/sass/style.scss', 'public/js/app.js'],
+            refresh: true,
+        }),
+    ],
+
+    build: {
+        outDir: 'public/build',  // Эта строка не обязательна, но может помочь
+    }
+});
